3bf7f425bbb4d39a8332f8878e96ebac680327fa
Sebastian <jaloszynskisebastian@gmail.com>
2022-10-18 22:01:57 +0200

    Dodane sprawdzanie czy czas oczekiwanie na dostawe < niz czas dojazdu ( wyswietla sie blad)

--- rozwiazanie.cpp
+++ rozwiazanie.cpp
@@ -17,25 +17,27 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <math.h>
 
 using namespace std;
 
+int road(int a, int b){
+    double len;
+    return len = sqrt(pow(a,2)+pow(b,2));
+}
+
+
 int main(){
     ifstream indata;
-    indata.open("test_case.txt");
+    indata.open("test_case2.txt");
     
     string value;
     string problem_name;
     int lines = 1,K,Q; 
     int i,x,y,q,e,l,d; 
+    int number_of_consuments, number_of_routes;
 
-    vector<int> custNO;
-    vector<int> Xcord;
-    vector<int> Ycord;
-    vector<int> demand;
-    vector<int> ready;
-    vector<int> duo;
-    vector<int> service;
+    vector<vector<int>> array;
 
     if(!indata)
     {
@@ -43,11 +45,9 @@ int main(){
     }
     else
     {
-        while (!indata.eof())
+                while (!indata.eof())
         {   
             getline(indata,value);
-            cout << lines << " ";
-            cout << value << endl;
             lines++;
             if(lines == 2){
                 problem_name = value;
@@ -57,20 +57,43 @@ int main(){
             }
             if(lines >= 10){
                 indata >> i >> x >> y >> q >> e >> l >> d;
-                custNO.push_back(i);
-                Xcord.push_back(x);
-                Ycord.push_back(y);
-                demand.push_back(q);
-                ready.push_back(e);
-                duo.push_back(l);
-                service.push_back(d);
+                vector<int> v1;
+                v1.push_back(i);
+                v1.push_back(x);
+                v1.push_back(y);
+                v1.push_back(q);
+                v1.push_back(e);
+                v1.push_back(l);
+                v1.push_back(d);   
+                array.push_back(v1);             
             }
         }
-        cout << problem_name << " " << K << " " << Q << endl; 
         indata.close();
 
-        for(int j=0;j<custNO.size();j++){
-            cout << custNO[j] << " " << Xcord[j] << " " << Ycord[j] << " " << demand[j] << " " << ready[j] << " " << duo[j] << " " << service[j] << endl;
+        number_of_consuments = array.size()-1;
+        
+        /*
+        cout << problem_name << " " << K << " " << Q << endl;
+
+        for(int j=0;j<array.size();j++)
+        {
+            for(int k=0; k < 7; k++)
+            {
+                cout << array[j][k] << " ";
+            }
+            cout << endl;
+        }
+        */
+        
+        
+    }
+    double road_time;
+    for(int j=1;j <= number_of_consuments; j++){
+        road_time = road(array[0][1]-array[j][1],array[0][2]-array[j][2]);
+        if(array[j][4] < road_time){
+            number_of_routes = -1;
+            break;
         }
     }
+    cout << number_of_routes << endl;
 }
